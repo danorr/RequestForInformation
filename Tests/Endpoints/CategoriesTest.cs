c15b50a94800cf7b9723c95c7a241a344d17b27e
Dan Orr NZ <danorr79+ubuntu@gmail.com>
2021-03-06 15:53:06 +1300

    Add readme instructions

--- Tests/Endpoints/CategoriesTest.cs
+++ Tests/Endpoints/CategoriesTest.cs
@@ -17,34 +17,32 @@ namespace Tests
         public async void GetDetailsVerifyNameCanRelistPromotions()
         {
             // Act
-            var baseUrl = "https://api.tmsandbox.co.nz/v1/";
-            var requestUrl = $"{baseUrl}Categories/6327/Details.json?catalogue=false";
-            var response = await client.GetAsync(baseUrl);
+            var baseUrl = "https://api.tmsandbox.co.nz/v1";
+            var requestUrl = $"{baseUrl}/Categories/6327/Details.json?catalogue=false";
+            var response = await client.GetAsync(requestUrl);
 
             if (!response.IsSuccessStatusCode)
             {
-                return;
+                Assert.True(false, "Request was not successful.");
             }
-
             // Arrange
             var body = await response.Content.ReadAsStringAsync();
             var content = JsonConvert.DeserializeObject<Category>(body);
 
             var name = content?.Name;
             var promoDescription = from promotion in content?.Promotions
-                        where promotion.Name == "Gallery"
-                        select promotion.Description;
-            bool canRelist = content.CanRelist;
+                                   where promotion.Name == "Gallery"
+                                   select promotion.Description;
+            var canRelist = content.CanRelist;
 
-            string expectedName = "Carbon credits";
-            string expectedDescription = "2x larger image";
-            bool expectedCanRelist = true;
+            var expectedName = "Carbon credits";
+            var expectedCanRelist = true;
+            var expectedDescription = "2x larger image";
 
             // Assert
             Assert.Equal(name, expectedName);
             Assert.Equal(true, expectedCanRelist);
             Assert.Equal(false, promoDescription.Contains(expectedDescription));
-
         }
     }
 }
